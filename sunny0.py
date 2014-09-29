3954d87e6f2c3c5f7c713cb5fc874a8e140494ef
sunnyme <sunny2891720@gmail.com>
2014-09-29 15:14:13 +0800

    sunny
    sunny

---
+++ sunny0.py
@@ -0,0 +1,8 @@
+"$(FULL_CURRENT_PATH)"
+
+print (3, -1, 3.14159, -2.8)
+
+
+print (type(3))
+print (type(3.14159))
+print (type(3.0))
---
+++ sunny1.py
@@ -0,0 +1,2 @@
+name = input('What is you name?\n')
+print('You are so handsome, %s.'% name)
