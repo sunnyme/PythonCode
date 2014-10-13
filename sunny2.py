060056a38fec253455c6794369faa91c979c967c
sunnyme <sunny2891720@gmail.com>
2014-10-13 14:41:35 +0800

    sunny10/13
    sunny10/13

--- sunny2.py
+++ sunny2.py
@@ -2,7 +2,7 @@ x = int(input("Please enter an integer:"))
 
 if x < 0:
     x = 0
-	print("Negative changed to zero")
+    print("Negative changed to zero")
 elif x == 0:
     print("Zero")
 elif x == 1:
