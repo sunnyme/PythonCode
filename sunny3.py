df75f308d17a3fff3fb473d475b1f44e002bd5fb
sunnyme <sunny2891720@gmail.com>
2014-10-13 15:13:55 +0800

    sunny10/13-2
    sunny10/13-2

---
+++ sunny3.py
@@ -0,0 +1,13 @@
+#temperature = 200
+#while temperature > 10:
+    #print(temperature)
+    #temperature = temperature - 1
+	
+#print('The tea is cool enough.')
+#i = 5
+#while i < 9:
+    #print(i)
+    #i = i + 2
+for x in range(1, 10):
+    for y in range(1, 10):
+        print(x , '*' , y , '=' , x*y)
